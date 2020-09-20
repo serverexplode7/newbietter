6c03f2c7b2df1cb19ceafaca744ad52ed52eed56
serverexplode7 <serverexplode7@naver.com>
2020-09-20 13:11:22 +0900

    edit delete

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
-import { auth } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
@@ -21,11 +16,11 @@ function App() {
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
-        "Initializing...."
+        "Initializing..."
       )}
-      <footer>&copy; {new Date().getFullYear()} newbietter</footer>
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
 }
